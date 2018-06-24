d2d7cbe73be9dbb067efe2e722b5fd18ae58eb5b
Marcus Almeida <s030827@gmail.com>
2018-06-24 10:42:27 -0300

    Listen data

--- routes/users.js
+++ routes/users.js
@@ -7,46 +7,43 @@ let db = new NeDB({
 });
 
 module.exports = (app)=>{
+
   app.get('/users', (req, res)=>{
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-      users:[{
-        name:'Marcus',
-        email: 'marcus@gmail.com',
-        id: 1
-      }]
-    });
+      db.find({}).sort({name:1}).exec((err, users)=>{
+          if (err) {
+            console.log(`error: ${err}`);
+            res.status(400).json({
+              error: err
+            });
+          } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+              users
+            });
+          } 
+      });
   });
 
-  app.post('/users', (req, res)=>{
-
-    db.insert(req.body, (err, user)=>{
-
-        if (err) {
-          console.log(`error: ${err}`);
-          res.status(400).json({
-            error: err
-          });
-        } else {
-          res.status(200).json(user); 
-        }
-
-    });
-  });
-
+  
   app.get('/users/admin', (req, res)=>{
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-      users:[{
-        name:'Admin',
-        email: 'admin@gmail.com',
-        id: 2
-      }]
-    });
+  
+      db.find({}).sort({name:1}).exec((err, users)=>{
+          
+          if (err) {
+            console.log(`error: ${err}`);
+            res.status(400).json({
+              error: err
+            });
+          } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+              users
+            });
+              
+          }
+      });
   });
-
 };
