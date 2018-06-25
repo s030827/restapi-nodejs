212347531d6a62636d4afb4d9d2fbd5c26cc284b
Marcus Almeida <s030827@gmail.com>
2018-06-25 09:41:42 -0300

    Code refactory

--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,13 @@ let db = new NeDB({
 
 module.exports = (app)=>{
 
-  app.get('/users', (req, res)=>{
+  let route = app.route('/users');
+
+  route.get((req, res)=>{
 
       db.find({}).sort({name:1}).exec((err, users)=>{
           if (err) {
-            console.log(`error: ${err}`);
-            res.status(400).json({
-              error: err
-            });
+            app.utils.error.send(err, req, res);
           } else {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
@@ -27,22 +26,14 @@ module.exports = (app)=>{
   });
 
   
-  app.get('/users/admin', (req, res)=>{
+  route.post((req, res)=>{
   
-      db.find({}).sort({name:1}).exec((err, users)=>{
+      db.insert(req.body, (err, user)=>{
           
           if (err) {
-            console.log(`error: ${err}`);
-            res.status(400).json({
-              error: err
-            });
+            app.utils.error.send(err, req, res);
           } else {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({
-              users
-            });
-              
+            res.status(200).json(user);
           }
       });
   });
